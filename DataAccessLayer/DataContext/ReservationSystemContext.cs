baa413fa492be5d4d674e54ef1f350792f14b242
Dmitry Mashkov <b1iz0@mail.ru>
2021-04-22 13:48:08 +0300

    Created initial context for ReservationSystem Db
    Context for our db was added and filled with DbSets of entities. There are: Roles, Users, AirplaneTypes, Airplanes, Flights, Countries, Cities, Airports.

---
+++ DataAccessLayer/DataContext/ReservationSystemContext.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.DataContext
+{
+    public class ReservationSystemContext : DbContext
+    {
+        public ReservationSystemContext(DbContextOptions<ReservationSystemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<AirplaneType> AirplaneTypes { get; set; }
+        public DbSet<Airplane> Airplanes { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Airport> Airports { get; set; }
+    }
+}
