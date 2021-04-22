53e6b47d8e63afb4c90f200d3395960b284131b1
Dmitry Mashkov <b1iz0@mail.ru>
2021-04-22 16:47:50 +0300

    -Created Repository for Airplane
    -Created Repository for AirplaneType

---
+++ DataAccessLayer/Repositories/AirplaneRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer.DataContext;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AirplaneRepository : IRepository<Airplane>
+    {
+        private ReservationSystemContext db;
+
+        public AirplaneRepository(ReservationSystemContext context)
+        {
+            this.db = context;
+        }
+        public IEnumerable<Airplane> GetAll()
+        {
+            return db.Airplanes;
+        }
+
+        public Airplane Get(int id)
+        {
+            return db.Airplanes.Find(id);
+        }
+
+        public IEnumerable<Airplane> Find(Func<Airplane, bool> predicate)
+        {
+            return db.Airplanes.Where(predicate).ToList();
+        }
+
+        public void Create(Airplane item)
+        {
+            db.Airplanes.Add(item);
+        }
+
+        public void Update(Airplane item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            Airplane airplane = db.Airplanes.Find(id);
+            if (airplane != null)
+            {
+                db.Airplanes.Remove(airplane);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/AirplaneTypeRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer.DataContext;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AirplaneTypeRepository : IRepository<AirplaneType>
+    {
+        private ReservationSystemContext db;
+
+        public AirplaneTypeRepository(ReservationSystemContext context)
+        {
+            this.db = context;
+        }
+        public IEnumerable<AirplaneType> GetAll()
+        {
+            return db.AirplaneTypes;
+        }
+
+        public AirplaneType Get(int id)
+        {
+            return db.AirplaneTypes.Find(id);
+        }
+
+        public IEnumerable<AirplaneType> Find(Func<AirplaneType, bool> predicate)
+        {
+            return db.AirplaneTypes.Where(predicate).ToList();
+        }
+
+        public void Create(AirplaneType item)
+        {
+            db.AirplaneTypes.Add(item);
+        }
+
+        public void Update(AirplaneType item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            AirplaneType type = db.AirplaneTypes.Find(id);
+            if (type != null)
+            {
+                db.AirplaneTypes.Remove(type);
+            }
+        }
+    }
+}
